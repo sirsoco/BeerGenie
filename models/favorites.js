d6d526a720cec6dfb111d62d60cfab93e44644d2
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-24 13:24:14 -0500

    added favorites.js model

---
+++ models/favorites.js
@@ -0,0 +1,23 @@
+
+var Sequelize = require("sequelize");
+
+var sequelize = require("../config/connection.js");
+
+// Creates a "Character" model that matches up with DB
+var Favorites = sequelize.define("beers", {
+
+  beer_id: Sequelize.STRING,
+
+  user_id: Sequelize.INTEGER
+
+ 
+}, {
+
+  freezeTableName: true
+});
+
+
+Character.sync();
+
+
+module.exports = Favorites;
