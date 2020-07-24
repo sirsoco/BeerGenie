708adbd0ffe051cd2565533eb565b583f1fc18ab
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-24 11:51:23 -0500

    added beergenie_db database

--- models/schema.sql
+++ models/schema.sql
@@ -1,1 +1,14 @@
-DROP DATABASE 
+DROP DATABASE IF EXISTS beergenie_db;
+CREATE DATABASE beergenie_db;
+
+USE beergenie_db;
+
+CREATE TABLE users(
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR (255), 
+last_name VARCHAR (255),
+beer_id int(21) NOT NULL,
+PRIMARY KEY(id)
+);
+
+
