4c3d4c8f090969f49320ececb07108046219b681
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-27 10:54:01 -0500

    removed Rank Routes

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -18,25 +18,7 @@ module.exports = function(app) {
       }); 
 });
 
-// Retrieves ranks and adds what user Ranked 
-app.get('/api/rank', function(req,res) {
-  var query = {};
-  if(req.query.rank_id) {
-    query.RankId = req.query.rank_id
-  }
-  db.Rank.findAll({
-    where:query,
-    include: [db.User] 
-  }).then(function(dbRank) {
-      res.json(dbRank)
-      console.log(dbRank)
-    }).catch(function (err) {
-      res.status(401).json(err);
-    });
-  });
 
- //@Ben @Emilee: Issue was that we were not using Params on Post 
- 
  // Retrieves saved beers by id 
  // Displays only User who added rank
 
@@ -53,22 +35,6 @@ app.get("/api/favorites/:id", function(req, res) {db.Favorite.findOne({
       }); 
 });
 
-//Retrieves individuals ranks
-// Displays only User who added rank 
-app.get('/api/rank/:id', function(req,res) {db.
-Rank.findOne({
-      where: {
-        id: req.params.id
-    },
-    include: [db.User]
-  }).then(function(dbRank) {
-      console.log(dbRank)
-      res.json(dbRank);
-  }).catch(function (err) {
-      res.status(401).json(err);
-    });
-});
-
 // Adds a favorite beer to the database 
 app.post("/api/favorites", function(req, res) {
     db.Favorite.create(req.body).then(function(dbFavorite) {
@@ -77,14 +43,6 @@ app.post("/api/favorites", function(req, res) {
     }).catch( function (err) {
       res.status(401).json(err)
     });
-});
-
-// Adds a rank to a favorite beer 
-  app.post("/api/rank", function(req, res) {
-    db.Rank.create(req.body).then(function(dbRank) {
-    res.json(dbRank)
-    console.log(dbRank)
-  });
 });
 
 // Deletes a favorite beer in the database
@@ -99,15 +57,5 @@ app.post("/api/favorites", function(req, res) {
     });
   });
 
-  app.delete('/api/rank/:id', function(req, res) {
-    db.Rank.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbRank) {
-      res.json(dbRank);
-      console.log('Rank Delete')
-    });
-  });
-
 };
+
