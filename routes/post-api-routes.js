4170539c594d886b277e1122dc5949b7dae93969
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-27 00:13:21 -0500

    added GET for rank - only for showing UserId not FavoritesId

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -17,9 +17,29 @@ module.exports = function(app) {
         res.status(401).json(err);
       }); 
 });
+
+// Retrieves ranks and adds what user Ranked 
+app.get('/api/rank', function(req,res) {
+  var query = {};
+  if(req.query.rank_id) {
+    query.RankId = req.query.rank_id
+  }
+  db.Rank.findAll({
+    where:query,
+    include: [db.User] 
+  }).then(function(dbRank) {
+      res.json(dbRank)
+      console.log(dbRank)
+    }).catch(function (err) {
+      res.status(401).json(err);
+    });
+  });
+
  //@Ben @Emilee: Issue was that we were not using Params on Post 
  
  // Retrieves saved beers by id 
+ // Displays only User who added rank
+ 
 app.get("/api/favorites/:id", function(req, res) {db.Favorite.findOne({
         where: {
           id: req.params.id
@@ -38,9 +58,12 @@ app.post("/api/favorites", function(req, res) {
     db.Favorite.create(req.body).then(function(dbFavorite) {
         res.json(dbFavorite)
         console.log(dbFavorite)
+    }).catch( function (err) {
+      res.status(401).json(err)
     });
 });
 
+// Adds a rank to a favorite beer 
   app.post("/api/rank", function(req, res) {
     db.Rank.create(req.body).then(function(dbRank) {
     res.json(dbRank)
@@ -48,7 +71,6 @@ app.post("/api/favorites", function(req, res) {
   });
 });
 
- 
 // Deletes a favorite beer in the database
  app.delete("/api/favorites/:id", function(req, res) {
     db.Favorite.destroy({
@@ -61,4 +83,4 @@ app.post("/api/favorites", function(req, res) {
     });
   });
 
-}
+};
