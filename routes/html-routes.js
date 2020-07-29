0452f6123884c8b6c426904576f649dba2d1e18c
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-07-29 16:24:01 -0500

    fixed /index html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,12 +28,14 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
-<<<<<<< HEAD
   app.get('/search', isAuthenticated, function(req, res){
     res.sendFile(path.join(__dirname, '../public/search.html'))
   });
-=======
+
+  app.get('/index', isAuthenticated, function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  });
+
   
->>>>>>> 5daff9da19bbffbdd4fa542160e3ad0f8c3be7cf
 
 };
