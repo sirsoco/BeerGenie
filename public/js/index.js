27c70a8d51791db6818f12e5b609bd14276166d3
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-08-09 10:33:38 -0500

    working search route by beername

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
-    // refrences to form and searchh params
-    var searchForm  = $('form.search');
-    var qInput = $('input.q');
+$(document).ready(function () {
+  // refrences to form and searchh params
+  var searchForm = $("form.search");
+  var qInput = $("input.q");
 
-// event listener for search
-console.log("index.js");
-searchForm.on('submit', function(event) {
+  // event listener for search
+  console.log("index.js");
+  searchForm.on("submit", function (event) {
     event.preventDefault();
     console.log("searched");
+    var beername = { q: qInput.val() };
+    req.body.beername;
+    app.post("/api/search/:beername", function () {
+      //selecting serch parameters
 
-    //selecting serch parameters
-    var q = { q: qInput.val() };
-    return console.log(JSON.stringify(q));
-
-    // need condition to alert if search not entered 
-})
+      console.log(q);
+      // return console.log(JSON.stringify(q));
+      // need condition to alert if search not entered
+    });
+  });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,45 +1,38 @@
 // requiring necessary npm packages
-const axios = require('axios');
-
+const axios = require("axios");
 // requiring passport as we've configured it
-var passport = require('../config/passport');
-
+var passport = require("../config/passport");
 //beer lib
-var Beer = require('../lib/beer.js');
-
+var Beer = require("../lib/beer.js");
 module.exports = function (app) {
-
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post('/api/login', passport.authenticate('local'), function (req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
-
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post('/api/signup', function (req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password,
     })
       .then(function () {
-        res.redirect(307, '/api/login');
+        res.redirect(307, "/api/login");
       })
       .catch(function (err) {
         res.status(401).json(err);
       });
   });
-
   // route for logging user out
-  app.get('/logout', function (req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
-    res.redirect('/login');
+    res.redirect("/login");
   });
-
   // route for getting some data about our user to be used client side
-  app.get('/api/user_data', function (req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -52,75 +45,77 @@ module.exports = function (app) {
       });
     }
   });
-
-
-  // route for getting a beer from BeerGenie API 
-  app.get('/api/favorites/:id', function (req, res) {
+  // route for getting a beer from BeerGenie API
+  app.get("/api/favorites/:id", function (req, res) {
     db.Favorite.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
-      include: [db.User]
-    }).then(function (dbFavorite) {
-      console.log(dbFavorite)
-      return res.json(dbFavorite);
+      include: [db.User],
     })
-    .catch(function (err) {
-      res.status(401).json(err);
-    });
+      .then(function (dbFavorite) {
+        console.log(dbFavorite);
+        return res.json(dbFavorite);
+      })
+      .catch(function (err) {
+        res.status(401).json(err);
+      });
   });
-
   // route to delete a beer from BeerGenie API
-  app.delete('/api/favorites/:id', function (req, res) {
+  app.delete("/api/favorites/:id", function (req, res) {
     db.Favorite.destroy({
       where: {
         id: req.params.id,
       },
     }).then(function (dbFavorite) {
       res.json(dbFavorite);
-      console.log('Beer Deleted');
+      console.log("Beer Deleted");
     });
-  })
-
-  // route to search using brewery API 
-  app.get('/api/search/:beername', function (req, res) {
-    var id = req.params.beername
-    const queryURL = `https://api.brewerydb.com/v2/beers?key=7873bf684e7db7e59e55ea9dbc1e8d4e&name=${id}`
+  });
+  app.post("/app/search/");
+  // route to search using brewery API
+  app.get("/breweerdb", function (req, res) {
+    const queryURL = `https://api.brewerydb.com/v2/beers?key=7873bf684e7db7e59e55ea9dbc1e8d4e&name=${id}`;
     console.log(queryURL);
-    axios.get(queryURL).then(function ({data}) {
-      console.log(data)
-      res.json(data)
-    }
-      // const repoNames = res.data.map(function(repo) {
-      // return repo.name;
-
-    ).catch(function (err) {
-      console.log(err)
-      res.status(401).json(err);
-    });
+    axios
+      .get(queryURL)
+      .then(
+        function ({ data }) {
+          console.log(data);
+          res.json(data);
+        }
+        // const repoNames = res.data.map(function(repo) {
+        // return repo.name;
+      )
+      .catch(function (err) {
+        console.log(err);
+        res.status(401).json(err);
+      });
   });
-
-//route to search using untappd API
-app.get('/api/search', function (req, res) {
- 
-  //selecting he string value of beer search
- 
-  //assiging the beer parameter for middleware function
-    
-  var config = {
-    method: 'get',
-    url: `https://api.untappd.com/v4/search/beer?q=blue&client_id=C07D8B1B31F42D67ABDAB78E49204B7E69788672&client_secret=2CBAEF54C119820777DADB2E0E6ACE4115E95295`,
-    headers: { }
-  };
+  // post route to search using untappd API
+  app.post("/api/search/:beername", function (req, res) {
+    console.log("beername");
+     //selecting he string value of beer search
+    //assiging the beer parameter for middleware function
+    var id = req.body.beername;
+    console.log(id);
   
-  axios(config)
-  .then(function ({response}) {
-  
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
+    // config 
+    var config = {
+      method: "get",
+      url: `https://api.untappd.com/v4/search/beer?q=${id}&client_id=C07D8B1B31F42D67ABDAB78E49204B7E69788672&client_secret=2CBAEF54C119820777DADB2E0E6ACE4115E95295`,
+      headers: {},
+    };
+    console.log(config.url);
+    axios(config)
+      .then(function (reply) {
+        var beers = JSON.stringify(reply.data.response.beers);
+        var beersArray = beers.split(",");
+        console.log(beersArray);
+        new Beer();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   });
-})
-
 };
