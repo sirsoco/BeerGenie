874adae2fcf16674d82cd2fcc07249ef39ecb07f
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-08-10 11:24:45 -0500

    reverted signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
