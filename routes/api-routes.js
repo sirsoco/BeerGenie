33b3a8eff4b0f62a477ba2c995c5317b1ff10054
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-08-10 10:09:53 -0500

    added beer to Favs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
     res.json(req.user);
   });
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // how we a our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post('/api/signup', function (req, res) {
     db.User.create({
