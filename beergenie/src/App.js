ccaf747cc5326319f563222708bedcf632b2518f
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-08-19 01:39:41 -0500

    added react nav bar;

--- beergenie/src/App.js
+++ beergenie/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home"
+
 
 
 function App() {
--- beergenie/src/pages/Home.js
+++ beergenie/src/pages/Home.js
@@ -1,6 +1,18 @@
-import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
-import API from "../utils/API";
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropDown'
+import API from '../utils/API';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import { FaBeer } from 'react-icons/fa';
+import './styles.css'
+
+
 
 class Home extends Component {
   state = {
@@ -34,7 +46,7 @@ class Home extends Component {
     this.getBeers();
   };
 
-  handleBeerSAve = (id) => {
+  handleBeerSave = (id) => {
     const beer = this.state.beers.find((beer) => beer.id === id);
 
     API.saveBeer({
@@ -44,40 +56,36 @@ class Home extends Component {
   };
   render() {
     return (
-      <>
-        <Navbar bg="light">
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="light">
-          <Navbar.Brand>Brand text</Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="dark">
-          <Navbar.Brand href="#home">
-            <img
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        </Navbar>
-        <br />
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            React Bootstrap
-          </Navbar.Brand>
-        </Navbar>
-      </>
+ <Container>
+  <Row>
+    <Col lg>
+    <Navbar bg="dark" expand="lg">
+  <Navbar.Brand href="#home"> <h2><FaBeer size={30} />BeerGeenie</h2></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline handleInputChange ={this.handleInputChange} handleFormSubmit ={this.handleFormSubmit}
+    q={this.state.q}>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+ 
+  </Col>
+  </Row>
+</Container>
+        
     );
   }
 }
