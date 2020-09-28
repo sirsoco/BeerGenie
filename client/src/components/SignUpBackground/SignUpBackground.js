21c2727b3494b0dd3139403f5347a7023f7eb8d4
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-09-28 15:48:49 -0500

    working firebase auth and redirect

---
+++ client/src/components/SignUpBackground/SignUpBackground.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import App from '../../App'
+export default function SignUpBackground() {
+    return(
+        <div className="SignUpBackground">
+            <img src={'./signupBackground.jpeg'}></img>
+        </div>
+    )
+}
